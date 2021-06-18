f37846aec4bfad0fd35059da1f2f0816eb471f31
Filipe Vale <fmmvale@hotmail.com>
2021-06-18 20:43:53 +0100

    init project

---
+++ gestTrack/gestTrack/F_principal.vb
@@ -0,0 +1,5 @@
+﻿Public Class F_principal
+    Private Sub F_principal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ gestTrack/gestTrack/Login.vb
@@ -0,0 +1,8 @@
+﻿Public Class Initial
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Me.Hide()
+        F_principal.ShowDialog()
+
+        Me.Close()
+    End Sub
+End Class
